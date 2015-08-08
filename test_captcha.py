1861a5bddfcedd8531b8f4d8c0904df537b28019
Kanokgan Hanmoungtham <kanokgan@gmail.com>
2015-08-08 13:30:58 +0700

    add testcase

--- test_captcha.py
+++ test_captcha.py
@@ -6,12 +6,30 @@ class TestFirstPatternLeftOperand(unittest.TestCase):
 		captcha = Captcha(1,1,1,1)
 		self.assertEqual('1', captcha.left_operand())
 		return False;
+	
+	def test_2_should_be_2(self):
+		captcha = Captcha(1,2,1,1)
+		self.assertEqual('2', captcha.left_operand())
+		return False;
+
+
+class TestSecondPatternLeftOperand(unittest.TestCase):
+	
+	def test_1_should_be_one(self):
+		captcha = Captcha(2,1,1,1)
+		self.assertEqual('one', captcha.left_operand())
+
 
 
 class Captcha():
 
 	def __init__(self, pattern, left, operand, right):
-		pass
+		self.pattern = pattern
+		self.left = left
 
 	def left_operand(self):
-		return '1'
+
+		if(self.pattern == 2):
+			return 'one'
+
+		return str(self.left)
