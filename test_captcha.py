88d0676d7a28241f0d4ae5fa30e3fbf2e259dcfc
Kanokgan Hanmoungtham <kanokgan@gmail.com>
2015-08-08 13:45:04 +0700

    Cleanup test case

--- test_captcha.py
+++ test_captcha.py
@@ -2,21 +2,31 @@ import unittest
 
 class TestFirstPatternLeftOperand(unittest.TestCase):
 	
+	DUMMY_PATTERN = 1
+	DUMMY_OPERATOR = 1
+	DUMMY_LEFT = 1
+	DUMMY_RIGHT = 1
+
 	def test_1_should_be_1(self):
-		captcha = Captcha(1,1,1,1)
+		captcha = Captcha(self.DUMMY_PATTERN, 1, self.DUMMY_OPERATOR, self.DUMMY_RIGHT)
 		self.assertEqual('1', captcha.left_operand())
 		return False;
 	
 	def test_2_should_be_2(self):
-		captcha = Captcha(1,2,1,1)
+		captcha = Captcha(self.DUMMY_PATTERN, 2, self.DUMMY_OPERATOR, self.DUMMY_RIGHT)
 		self.assertEqual('2', captcha.left_operand())
 		return False;
 
 
 class TestSecondPatternLeftOperand(unittest.TestCase):
 	
+	DUMMY_PATTERN = 2
+	DUMMY_OPERATOR = 1
+	DUMMY_LEFT = 1
+	DUMMY_RIGHT = 1
+	
 	def test_1_should_be_one(self):
-		captcha = Captcha(2,1,1,1)
+		captcha = Captcha(self.DUMMY_PATTERN, 1, self.DUMMY_OPERATOR, self.DUMMY_RIGHT)
 		self.assertEqual('one', captcha.left_operand())
 
 
@@ -33,3 +43,31 @@ class Captcha():
 			return 'one'
 
 		return str(self.left)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
