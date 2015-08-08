919baf218e15742e50023ab5448a82aa5b705013
Kanokgan Hanmoungtham <kanokgan@gmail.com>
2015-08-08 13:55:07 +0700

    Added TestSecondPatternRightOperand

--- test_captcha.py
+++ test_captcha.py
@@ -30,20 +30,53 @@ class TestSecondPatternLeftOperand(unittest.TestCase):
 		self.assertEqual('one', captcha.left_operand())
 
 
+class TestFirstPatternRightOperand(unittest.TestCase):
+	
+	DUMMY_PATTERN = 1
+	DUMMY_OPERATOR = 1
+	DUMMY_LEFT = 1
+	DUMMY_RIGHT = 1
+
+	def test_1_should_be_1(self):
+		captcha = Captcha(self.DUMMY_PATTERN, self.DUMMY_LEFT, self.DUMMY_OPERATOR, 1)
+		self.assertEqual('1', captcha.right_operand())
+
+
+class TestSecondPatternRightOperand(unittest.TestCase):
+	
+	DUMMY_PATTERN = 2
+	DUMMY_OPERATOR = 1
+	DUMMY_LEFT = 1
+	DUMMY_RIGHT = 1
+
+	def test_1_should_be_1(self):
+		captcha = Captcha(self.DUMMY_PATTERN, self.DUMMY_LEFT, self.DUMMY_OPERATOR, 1)
+		self.assertEqual('one', captcha.right_operand())
+
+
 
 class Captcha():
+
+	numberStr = ['zero', 'one', 'two', 'tree', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 	def __init__(self, pattern, left, operand, right):
 		self.pattern = pattern
 		self.left = left
+		self.right = right
 
 	def left_operand(self):
 
 		if(self.pattern == 2):
-			return 'one'
+			return self.numberStr[self.left]
 
 		return str(self.left)
 
+	def right_operand(self):
+
+		if(self.pattern == 2):
+			return self.numberStr[self.right]
+
+		return str(self.right)
 
 
 
@@ -70,4 +103,5 @@ class Captcha():
 
 
 
-		
+
+
